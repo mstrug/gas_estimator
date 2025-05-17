f99cc771a912d65734219f36e5cd87aebc5b9e99
Michal Strug <michal.strug@op.pl>
2025-05-17 14:16:51 +0200

    Added frontend page

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,11 @@
 use crate::{App, domain_data_model};
-use axum::{Json, extract::State, http::StatusCode};
+use axum::{Json, extract::State, http::StatusCode, response::Html};
 use serde::Deserialize;
+use serde::Deserializer;
 
-pub async fn root() -> &'static str {
+pub async fn root() -> Html<&'static str> {
     log::info!("Requested root");
-    "Wrong API call"
+    Html::from(include_str!("html/main.html"))
 }
 
 pub async fn version() -> &'static str {
@@ -19,28 +20,36 @@ pub struct EstimateInputData {
     /// Transaction sender account in form of hexadecimal string.
     /// Field is optional.
     /// Example: 0xabc1234567890abc1234567890abc1234567890f
+    #[serde(deserialize_with = "non_empty_str")]
     from: Option<String>,
 
     /// Transaction destination in form of hexadecimal string.
     /// Example: 0xabc1234567890abc1234567890abc1234567890f
     to: String,
 
-    /// Ether to send in the transaction in form of wei or number with unit.
+    /// Ether to send in the transaction in form of hexadecimal string.
     /// Field is optional.
-    /// Examples: 10, 0.1ether, 1gwei
+    /// Examples: 0x1
+    #[serde(deserialize_with = "non_empty_str")]
     value: Option<String>,
 
     /// Call data in form of hexadecimal string.
     /// Field is optional.
-    /// Example: 0x095ea7b3000000000000000000000000
+    /// Example: 0xd0e30db0
+    #[serde(deserialize_with = "non_empty_str")]
     data: Option<String>,
 
     /// Block at which estimation should be done. Hexadeciaml value or string: earliest, finalized, safe, latest, or pending.
     /// Field is optional, defaults to 'latest'.
     /// Example: 0x1572338
+    #[serde(deserialize_with = "non_empty_str")]
     block: Option<String>,
 }
-
+fn non_empty_str<'de, D: Deserializer<'de>>(d: D) -> Result<Option<String>, D::Error> {
+    use serde::Deserialize;
+    let o: Option<String> = Option::deserialize(d)?;
+    Ok(o.filter(|s| !s.is_empty()))
+}
 impl Into<domain_data_model::EstimateGas> for EstimateInputData {
     fn into(self) -> domain_data_model::EstimateGas {
         domain_data_model::EstimateGas {
